be665428880b936ed47a8d7801ef1e184422851e
Vinko Kodzoman <vinko.kodzoman@fer.hr>
2015-10-24 22:14:29 +0200

    AND and OR working perfectly with exact matching. SQL string printing is enabled as well.

--- NMBP/project1/search_engine/app/controllers/search_page_controller.rb
+++ NMBP/project1/search_engine/app/controllers/search_page_controller.rb
@@ -6,13 +6,34 @@ class SearchPageController < ApplicationController
   end
   
   def show
-
   # Check if the users typed anything in the search bar
   if params[:search] != "" 
+  
+    # Split the search string and remove ""
+    values = params[:search].split();
+	values.each { |x| x.delete! '"'}
+	
+	# Set the logical parameters
+	if params[:logic] == "and"
+		logicParam = " AND "
+	end
+	if params[:logic] == "or"
+	    logicParam = " OR "
+	end
+	
+	# Build the sql string core
+	for i in 0..(values.length-1)
+		if i != values.length-1
+			sqlSubString = sqlSubString.to_s + " text LIKE '%" + values[i].to_s + "%'" + logicParam.to_s
+		else
+			sqlSubString = sqlSubString.to_s + " text LIKE '%" + values[i].to_s + "%'"
+		end
+	end
+	
     # Exact search
     if params[:method]  == "exact"
-		sqlString = "select * from documents where text like '%" + params[:search].to_s + "%'";
-		@documents = ActiveRecord::Base.connection.execute(sqlString).values
+		@sql = "SELECT * FROM documents WHERE" + sqlSubString
+		@documents = ActiveRecord::Base.connection.execute(@sql).values
 	end
   end
   
