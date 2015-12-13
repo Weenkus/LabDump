df55647a0b14b8e2c3a0a58b54ad5be1dc7f5c9d
Vinko <vinko.kodzoman@fer.hr>
2015-12-13 12:53:35 +0100

    Fixed the program so it parses the input file correctly.

--- Bioinformatics/Bioinformatics 1/PatternCount/PatternCount.cpp
+++ Bioinformatics/Bioinformatics 1/PatternCount/PatternCount.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <iostream>
 
 int patternCount(std::string genome, std::string pattern);
 
@@ -8,12 +9,12 @@ int main() {
 	
 	// Open and read the file
 	std::ifstream fileHandel("dataset_2_6.txt");
-	std::string genome((std::istreambuf_iterator<char>(fileHandel)),
-		std::istreambuf_iterator<char>());
-
-	std::string pattern = "ATA";
+	std::string genome, pattern;
+	
+	std::getline(fileHandel, genome);
+	std::getline(fileHandel, pattern);
+		
 	int ptCountS = patternCount(genome, pattern);
-
 	std::cout << "The pattern " << pattern << " is repeted " << ptCountS << " times in the input genome.\n";
 	
 	getchar();
