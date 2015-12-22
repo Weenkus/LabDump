706d90a3659049d802ebf89451af329a58c3edfa
Vinko <vinko.kodzoman@fer.hr>
2015-12-22 21:31:20 +0100

    Added essential classses.

--- Objektno oblikovanje/DZ2/StockExchange/StockExchange/StockExchange.cs
+++ Objektno oblikovanje/DZ2/StockExchange/StockExchange/StockExchange.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace DrugaDomacaZadaca_Burza
 {
@@ -15,6 +14,11 @@ namespace DrugaDomacaZadaca_Burza
 
      public class StockExchange : IStockExchange
      {
+        private LinkedList<Portfolio> portoflios { get; set; }
+        private LinkedList<Stock> stocks { get; set; }
+        private LinkedList<Index> indexes { get; set; }
+
+
          public void ListStock(string inStockName, long inNumberOfShares, decimal inInitialPrice, DateTime inTimeStamp)
          {
              throw new NotImplementedException();
@@ -32,7 +36,7 @@ namespace DrugaDomacaZadaca_Burza
 
          public int NumberOfStocks()
          {
-             throw new NotImplementedException();
+            return this.stocks.Count();
          }
 
          public void SetStockPrice(string inStockName, DateTime inIimeStamp, decimal inStockValue)
@@ -87,7 +91,7 @@ namespace DrugaDomacaZadaca_Burza
 
          public int NumberOfIndices()
          {
-             throw new NotImplementedException();
+            return this.indexes.Count();
          }
 
          public int NumberOfStocksInIndex(string inIndexName)
@@ -117,7 +121,7 @@ namespace DrugaDomacaZadaca_Burza
 
          public int NumberOfPortfolios()
          {
-             throw new NotImplementedException();
+            return this.portoflios.Count();
          }
 
          public int NumberOfStocksInPortfolio(string inPortfolioID)
@@ -150,4 +154,26 @@ namespace DrugaDomacaZadaca_Burza
              throw new NotImplementedException();
          }
      }
+
+    public class Stock
+    {
+        private string name { get; set; }
+        private int number { get; set; }
+        private decimal value { get; set; }
+        private DateTime validTime { get; set; }
+    }
+
+    public class Index
+    {
+        private string name { get; set; }
+        private IndexTypes indexType { get; set; }
+        private LinkedList<Stock> stocks { get; set; }
+    }
+
+    public class Portfolio
+    {
+        private int id { get; set; }
+        private LinkedList<Stock> stocks { get; set; }
+    }
+
 }
