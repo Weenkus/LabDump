52ab7b88fe82361156abc99502006b3b05015e88
Vinko Kodzoman <vinko.kodzoman@fer.hr>
2016-01-18 14:09:28 +0100

    Adds the second map reduce algorithm. MR2 gives author with 10 most frequent words in their blog post.

--- NMBP/project3/MR/mr2.js
+++ NMBP/project3/MR/mr2.js
@@ -1,13 +1,47 @@
 var map = function() {
-	if (typeof this.comments !== 'undefined') {
-		emit( this.title, )
-	} else {
-		emit( Number(1/0) + "#" + this.title, this.title)
-	}                     
+	var words = this.content.split(" ")
+	var occurance = new Array()
+
+	var max = 0;
+	for( var i = 0; i < words.length; i++ ) {
+    	occurance[ words[ i ].toLowerCase() ] = ( occurance[ words[ i ].toLowerCase() ] || 0 ) + 1
+		if(occurance[ words[ i ].toLowerCase() ] > max)
+			max = occurance[ words[ i ].toLowerCase() ]
+	}
+
+	var topWords = new Array()
+	var insertedItems = 0
+	for(var i = max; i >= 0; i--) {
+		for (var key in occurance) {
+			if(occurance[key] == max) {
+				topWords[insertedItems] = key + " : " + occurance[key];
+				insertedItems++;	
+				if(insertedItems == 10)
+					break;	
+			}
+		}
+		if(insertedItems == 10)
+			break
+		max--
+	}
+
+	emit( this.title, topWords)
+
 };
 
 var reduce = function(key, values) {
-	return values
+	values.sort(function(a, b) {
+		a = a[1];
+		b = b[1];
+		return a < b ? -1 : (a > b ? 1 : 0);
+	});
+
+	var out = ""
+	for( var i = 0; i < 10; i++ ) {
+		out = out + values[i] + " "
+	}
+
+	return out
 };
 
 var fin = function (key, values) {
