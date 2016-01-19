8c0334a91baaaf4ff42c54645285aa7290a05d50
Vinko Kodzoman <vinko.kodzoman@fer.hr>
2016-01-19 22:56:39 +0100

    Adds final mapReduce algorithm, that's scalable and correct.

--- NMBP/project3/MR/mr2.js
+++ NMBP/project3/MR/mr2.js
@@ -1,5 +1,69 @@
 var map = function() {
-	var words = this.content.split(" ")
+	var words = this.content.split(/[\s,.!?]+/)
+	var occurance = new Array()
+
+	for( var i = 0; i < words.length; i++ ) {
+    	occurance[ words[ i ].toLowerCase() ] = ( occurance[ words[ i ].toLowerCase() ] || 0 ) + 1
+	}
+
+	for( var key in occurance) {
+		emit( this.author, key + " " + occurance[key])
+	}
+	
+};
+
+var reduce = function(key, values) {
+	var max = 0
+	for( var i = 0; i < values.length; i++ ) {
+		var decomp = values[i].split(" ")
+		if(decomp[1] > max)
+			max = decomp[1]
+	}
+
+	var topWords = new Array()
+	var insertedItems = 0
+	var out = ""
+	for(var i = max; i >= 0; i--) {
+		for( var j = 0; j < values.length; j++ ) {
+				var splitStr = values[j].split(" ")
+				if(splitStr[1] == max) {
+					out = out + values[j] + ", "
+					insertedItems++
+				}
+				if(insertedItems == 10)
+					break
+		}
+		if(insertedItems == 10)
+			break
+		max--
+	}
+	return out;
+};
+
+var fin = function (key, values) {
+	return values
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var map = function() {
+	var words = this.content.split(/[\s,.!?]+/)
 	var occurance = new Array()
 
 	var max = 0;
@@ -30,12 +94,6 @@ var map = function() {
 };
 
 var reduce = function(key, values) {
-	values.sort(function(a, b) {
-		a = a[1];
-		b = b[1];
-		return a < b ? -1 : (a > b ? 1 : 0);
-	});
-
 	var out = ""
 	for( var i = 0; i < 10; i++ ) {
 		out = out + values[i] + " "
