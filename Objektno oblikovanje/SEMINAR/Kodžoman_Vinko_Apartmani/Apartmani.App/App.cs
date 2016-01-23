e18354aff9910abb50a74e42ab14b32df4928ebb
Vinko <vinko.kodzoman@fer.hr>
2016-01-23 18:13:52 +0100

    Initialises the aprtman system.

---
+++ Objektno oblikovanje/SEMINAR/Kodžoman_Vinko_Apartmani/Apartmani.App/App.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apartmani.App
+{
+    class App
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
---
+++ Objektno oblikovanje/SEMINAR/Kodžoman_Vinko_Apartmani/Apartmani.DomainModel/Agent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Apartmani.DomainModel
+{
+    public class Agent
+    {
+        private int _id;
+        private String _name;
+        private String _lastName;
+    }
+}
