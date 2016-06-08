e5b4fc86c6febafb0faf8c1c633b80c5d680895a
Vinko <vinko.kodzoman@fer.hr>
2016-06-08 09:14:54 +0200

    hope xD

--- AVSP/lab4/DGIM_py/DGIM.py
+++ AVSP/lab4/DGIM_py/DGIM.py
@@ -8,6 +8,7 @@ TIMESTAMP = 0
 SIZE = 0
 TS = 1
 
+
 def main():
     N = 0
     buckets = []
@@ -37,22 +38,31 @@ def stream(line, N, buckets):
 
         k = 0
         number_of_ones = 0
+        wrote_to_output = False
+        if MODE == "LOCAL":
+            print('q_v:', query_value, buckets)
         for i, bucket in enumerate(buckets):
 
             if i != 0:
                 k += buckets[i-1][TS] - buckets[i][TS]
+                #print('k:', k)
 
-            if k > query_value:
-                #number_of_ones -= bucket[SIZE]
-                print(int(number_of_ones))
+            if k >= query_value:
+                number_of_ones -= buckets[i-1][SIZE]/2
+                print(int(number_of_ones), k)
+                wrote_to_output = True
+                break
             else:
                 number_of_ones += bucket[SIZE]
 
+        if not wrote_to_output:
+            output = int(number_of_ones) - int(buckets[-1][SIZE]/2)
+            print("K:", output)
     else:
         for c in line:
 
             if len(buckets) > 2 and (buckets[0][TS] - buckets[-1][TS]) > N:
-                buckets.remove(buckets[-1])
+                buckets.pop()
 
             if c == '1':
                 buckets.insert(0, [1, TIMESTAMP])
@@ -61,10 +71,9 @@ def stream(line, N, buckets):
 
                     if i > 1 and buckets[i][SIZE] == buckets[i-1][SIZE] == buckets[i-2][SIZE]:
                         buckets.remove(buckets[i])
-                        buckets[i-1][SIZE] = buckets[i-1][SIZE] * 2
+                        buckets[i-1][SIZE] *= 2
 
-
-            TIMESTAMP = TIMESTAMP + 1
+            TIMESTAMP += 1
 
     return buckets
 
